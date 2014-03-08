bbfc5bdcb8f1e9803beabcc3bedcc2655c0fdeb9
suhashm <mandahasa2626@gmail.com>
2014-03-08 22:58:05 +0530

    First Commit: Changes done till MongoDB

---
+++ server.js
@@ -0,0 +1,41 @@
+/**
+ * Created by shm2 on 3/4/14.
+ */
+
+var express = require('express'),
+    stylus = require('stylus');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+function compile(str, path){
+    return stylus(str).set('filename', path);
+}
+
+app.configure(function(){
+   app.set('views', __dirname + '/server/views');
+   app.set('view engine', 'jade');
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+   app.use(stylus.middleware(
+       {
+            src: __dirname + '/public',
+           compile: compile
+       }
+   ))
+
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/partials/:partialPath', function(req, res){
+    res.render('partials/'+req.params.partialPath);
+})
+
+app.get('*',function(req, res){
+    res.render('index');
+});
+
+var port = 3030;
+app.listen(port);
+console.log('Listening on Port '+port+' ....');
